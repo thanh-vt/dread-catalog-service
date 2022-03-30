c21b0c094334688ae9465a084a6f5dd75a846955
thanhvt <thanhvt@vissoft.vn>
2022-03-31 01:04:54 +0700

    update singleton

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,9 +4,15 @@ namespace App\Providers;
 
 use App\Repositories\CategoryRepository;
 use App\Repositories\Impl\CategoryRepositoryImpl;
+use App\Repositories\Impl\ProductRepositoryImpl;
+use App\Repositories\Impl\UserRepositoryImpl;
+use App\Repositories\ProductRepository;
+use App\Repositories\UserRepository;
 use App\Services\CategoryService;
 use App\Services\ConfigService;
 use App\Services\Impl\CategoryServiceImpl;
+use App\Services\Impl\ProductServiceImpl;
+use App\Services\ProductService;
 use Illuminate\Database\Events\QueryExecuted;
 use Illuminate\Support\ServiceProvider;
 use DB;
@@ -22,8 +28,16 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         $this->app->singleton(ConfigService::class);
+        $this->app->singleton(UserRepository::class, UserRepositoryImpl::class);
+
         $this->app->singleton(CategoryRepository::class, CategoryRepositoryImpl::class);
         $this->app->singleton(CategoryService::class, CategoryServiceImpl::class);
+
+        $this->app->singleton(CategoryRepository::class, CategoryRepositoryImpl::class);
+        $this->app->singleton(CategoryService::class, CategoryServiceImpl::class);
+
+        $this->app->singleton(ProductRepository::class, ProductRepositoryImpl::class);
+        $this->app->singleton(ProductService::class, ProductServiceImpl::class);
     }
 
     /**
